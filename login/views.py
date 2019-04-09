7839912fce5096438236b9295074726f09ee0c3c
Milan Svehla <milan.svehla@gmail.com>
2019-04-09 18:24:13 +0200

    beta

--- login/views.py
+++ login/views.py
@@ -41,7 +41,7 @@ def result(request):
     api = tweepy.API(auth)
 
     context = {
-        "ids": api.followers_ids()
+        "ids": api.friends_ids()
     }
 
     return render(request, 'login/result.html', context)

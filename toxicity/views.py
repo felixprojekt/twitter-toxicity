e263982aad76e2ab4634053a52a60f121dd96ca5
Milan Svehla <milan.svehla@gmail.com>
2019-04-22 23:06:57 +0200

    beta

--- toxicity/views.py
+++ toxicity/views.py
@@ -17,7 +17,7 @@ def user_toxicity(request, user_id):
     try:
         auth.get_access_token(verifier)
     except tweepy.TweepError:
-        print('Error! Failed to get access token.')
+        print(tweepy.TweepError)
 
     api = tweepy.API(auth)
 
